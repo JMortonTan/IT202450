b3dc342ea8284bc753353e249e97b0d483204559
Jonathan Tan <jmt26@njit.edu>
2020-05-30 01:24:29 -0400

    Playing with update user

--- public_html/PHPIntro/update_user.php
+++ public_html/PHPIntro/update_user.php
@@ -5,7 +5,7 @@ $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 try{
     $db = new PDO($connection_string, $dbuser, $dbpass);
 
-    #Delete all
+    #Will update EVERY original email
     $stmt = $db->prepare("UPDATE Users set email=:email WHERE email = :original");
 
     $stmt->bindValue(":email","newemail@test.com");
