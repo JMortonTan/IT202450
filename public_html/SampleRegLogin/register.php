22a54d55908f7a8b750813ae5d297e4389487079
Jonathan Tan <jmt26@njit.edu>
2020-06-05 17:46:12 -0400

    Finished registration

--- public_html/SampleRegLogin/register.php
+++ public_html/SampleRegLogin/register.php
@@ -18,11 +18,31 @@
 
 //Show user confirm
 if(isset($_POST["register"])){
-    if(isset($_POST["password"]) && isset($_POST["cpassword"])){
+    if(isset($_POST["password"]) && isset($_POST["cpassword"]) && isset($_POST["email"])){
         $password = $_POST["password"];
         $cpassword = $_POST["cpassword"];
         if($password == $cpassword){
-            echo "<div>Passwords Match</div>";
+            //echo "<div>Passwords Match</div>";
+            require("config.php");
+            $connection_string = "mysqli:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+            try{
+                $db = new PDO($connection_string, $dbuser, $dbpass);
+                $stmt = $db->prepare("INSERT INTO Users (email, password VALUES(:email, :password))");
+                $stmt->execute(array(
+                        ":email" => $email,
+                        ":password" => $password
+                ));
+                $e = $stmt->errorInfo();
+                if($e[0] != "00000"){
+                    echo var_export($e, true);
+                }
+                else{
+                    echo "<div>Successfully registered!</div>";
+                }
+            }
+            catch (Exception $e){
+                echo $e->getMessage();
+            }
             }
         else {
             echo "<div>Passwords don't match</div>";
