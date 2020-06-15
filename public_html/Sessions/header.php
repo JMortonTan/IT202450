ca7b8d2cdd494079f024c3c21832f9080708a18d
Jonathan Tan <jmt26@njit.edu>
2020-06-14 22:46:54 -0400

    consolidating changes under nav

--- public_html/Sessions/header.php
+++ public_html/Sessions/header.php
@@ -4,10 +4,10 @@
 </head>
 <nav>
     <ul>
-        <li><a id="menu" href="home.php">Home</a></li>
-        <li><a id="menu" href="login.php">Login</a></li>
-        <li><a id="menu" href="register.php">Registration</a></li>
-        <li><a id="menu" href="logout.php">Logout</a></li>
+        <li><a href="home.php">Home</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="register.php">Registration</a></li>
+        <li><a href="logout.php">Logout</a></li>
     </ul>
 </nav>
 <?php
