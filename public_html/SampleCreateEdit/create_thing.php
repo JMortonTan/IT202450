9c3be5ce47045a0c84ff7d2e06c8e5bd2522273d
Jonathan Tan <jmt26@njit.edu>
2020-06-21 16:56:50 -0400

    Code not performing like professors example. Debugging own typos

--- public_html/SampleCreateEdit/create_thing.php
+++ public_html/SampleCreateEdit/create_thing.php
@@ -22,7 +22,7 @@ if(isset($_POST["created"])){
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
         try{
             $db = new PDO($connection_string, $dbuser, $dbpass);
-            $stmt = $db->prepare("INSERT INTO THINGS (name, quantity) VALUE(:name, :quantity)");
+            $stmt = $db->prepare("INSERT INTO THINGS(name, quantity) VALUE(:name, :quantity)");
             $result = $stmt->execute(array(
                 ":name" => $name,
                 ":$quantity" => $quantity
