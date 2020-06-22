ab6c10c21a7f613d9ddce9697e351bb0bc645a1a
Jonathan Tan <jmt26@njit.edu>
2020-06-21 20:32:56 -0400

    Testing form for create HW5

--- public_html/Week5HW/create.php
+++ public_html/Week5HW/create.php
@@ -1,3 +1,13 @@
+<form method="POST">
+    <label for="Account">Account Number
+        <input type="text" id="acc_number" name="account_number" />
+    </label>
+    <label for="Account_Type">Account Type
+        <input type="number" id="acc_type" name="account_type" />
+    </label>
+    <input type="submit" name="created" value="Create Thing"/>
+</form>
+
 <?php
 require("common.inc.php");
 $db = getDB();
