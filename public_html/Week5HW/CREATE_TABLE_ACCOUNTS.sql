5b6487a413cd40e13de2eb8689b34ab05fb5949b
Jonathan Tan <jmt26@njit.edu>
2020-06-21 20:19:07 -0400

    HW 5, upload of sql script run.

---
+++ public_html/Week5HW/CREATE_TABLE_ACCOUNTS.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Accounts(
+    /*Internal (Primary Key)*/
+    id int AUTO_INCREMENT,
+    /*World account is 12 '0's*/
+    account_number varchar(12) NOT NULL,
+    /*Joined with Users table (Foreign Key)*/
+    user_id int,
+    /*Value 1: Checking, Value 2: Savings, Value 3: Loan*/
+    account_type int(1) default 0,
+    opened_date DATETIME default CURRENT_TIMESTAMP,
+    balance decimal(12,2) default 0.00,
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id) REFERENCES Users(id)
+)
