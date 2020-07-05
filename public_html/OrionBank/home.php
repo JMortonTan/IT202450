97d08833594400c2c07df724e78f58e699a2c75a
Jonathan Tan <jmt26@njit.edu>
2020-07-05 18:52:23 -0400

    testing vertical align fix

--- public_html/OrionBank/home.php
+++ public_html/OrionBank/home.php
@@ -6,7 +6,6 @@ include("header.php");
 echo '<p>Welcome to Orion Bank!</p>';
 if($logged_in){
     print $_SESSION["user"]["first_name"] . " " . $_SESSION["user"]["last_name"];
-    print 'You are logged in';
 } else {
     include 'login.php';
 }
