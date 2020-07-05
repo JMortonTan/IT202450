2808a48ebe5eae37407e1a14a25286a04168c7a0
Jonathan Tan <jmt26@njit.edu>
2020-07-05 15:46:08 -0400

    Trying to make some sense of old code

--- public_html/OrionBank/header.php
+++ public_html/OrionBank/header.php
@@ -2,20 +2,22 @@
     <title>Bank</title>
     <link rel="stylesheet" type="text/css" href="style.css"
 </head>
+<?php
+require("config.php");
+session_start();
+?>
 <nav id="menu">
     <ul>
-        <li><img src="media/Logo.png" alt="Orion Bank"></li>
+        <li><img src="media/Logo.png" id="logo" alt="Orion Bank"></li>
         <li><a href="home.php">Home</a></li>
         <li><a href="accounts.php">My Accounts</a></li>
         <li><a href="login.php">Login</a></li>
         <li><a href="register.php">Registration</a></li>
         <?php if(isset($_SESSION)):?>
-        <li><a href="logout.php">Login</a></li>
+            <li><a href="accounts.php">My Accounts</a></li>
+            <li><a href="logout.php">Logout</a></li>
         <?php endif;?>
     </ul>
 </nav>
-<?php
-    require("config.php");
-    session_start();
-?>
+
 
