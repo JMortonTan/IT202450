70f7254e0c60857c1c97ce543a0f3705b7ae3f2f
Jonathan Tan <jmt26@njit.edu>
2020-07-05 21:48:14 -0400

    print statements to see where it goes wrong.

--- public_html/OrionBank/openaccount.php
+++ public_html/OrionBank/openaccount.php
@@ -23,17 +23,24 @@ if(isset($_POST["created"])){
     $account_type = $_POST["account_type"];
     $user_id = $_SESSION["user"]["id"];
 
+    echo "Get ready to PDO!";
     $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
     if(!empty($account_number) && !empty($account_type)){
         require("includes/common.inc.php");
-        $db = new PDO($connection_string, $dbuser, $dbpass);
 
+        echo "Entered the if statement";
+        $db = new PDO($connection_string, $dbuser, $dbpass);
+        echo "PDO is legit";
         $stmt = $db->prepare("INSERT INTO Accounts (account_number, user_id, account_type) VALUES (:account_number, :user_id, :account_type)");
+
+        echo "Statement prepared";
         $result = $stmt->execute(array(
             ":account_number" => $account_number,
             ":user_id" => $user_id,
             ":account_type" => $account_type
         ));
+
+        echo "Statement executed";
         //Error Handling
         $e = $stmt->errorInfo();
         if($e[0] != "00000"){
