84111d9e1f01c7645a8266df295405406f41073e
Jonathan Tan <jmt26@njit.edu>
2020-07-05 22:45:36 -0400

    I'm at fault. Nobody else to blame.

--- public_html/OrionBank/makeitrain.php
+++ public_html/OrionBank/makeitrain.php
@@ -10,13 +10,15 @@ if (isset($_GET['account'])){
     $account_number = $_GET['account'];
 
     echo $account_number;
+    $query = file_get_contents("queries/MAKEITRAIN.sql");
 
     $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
     if(!empty($account_number)) {
         echo "I love PDO  <br>";
         $db = new PDO($connection_string, $dbuser, $dbpass);
         echo "It works everytime I want it to. <br>";
-        $stmt = $db->prepare("queries/MAKEITRAIN.sql");
+        $stmt = $db->prepare($query);
+        echo "Its not anyone's fault but mine.";
         $stmt->execute([":account_number" => $account_number]);
 
     }
