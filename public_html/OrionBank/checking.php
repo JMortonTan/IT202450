ed06edce5d2c206bf8527e25f8683bc9d288b9db
Jonathan Tan <jmt26@njit.edu>
2020-07-26 15:29:28 -0400

    test checking page sql pull

--- public_html/OrionBank/checking.php
+++ public_html/OrionBank/checking.php
@@ -7,8 +7,6 @@ include("header.php");
 <?php
 
 if (isset($_GET['account'])){
-    $account_number = $_GET['account'];
-
     echo $account_number;
     $query = file_get_contents("queries/SEARCH_TABLE_ACCOUNTS_ACCOUNTNUM.sql");
 
@@ -16,14 +14,35 @@ if (isset($_GET['account'])){
     try {
         $db = new PDO($connection_string, $dbuser, $dbpass);
         $stmt = $db->prepare($query);
-        $stmt->execute([":account_number" => $account_number]);
+        $result = $stmt->execute([":account_number" => $account_number]);
 
     } catch (Exception $e) {
         echo $e->getMessage();
     }
-
-
 }
 
+if(isset($result) && count($result) == 0){
+    echo "<h5>Account Number: </h5>";
+    echo "<h5>" . $result["account_number"] . "</h5>";
+
+    echo "<h5>Account Type: </h5>";
+    echo "<h5>";
+    switch ($result["account_type"]) {
+        case 1:
+            echo "Checking";
+            break;
+        case 2:
+            echo "Savings";
+            break;
+        case 3:
+            echo "Loan";
+            break;
+        default:
+            echo "There is an error";
+            break;
+    };
+    echo "</h5>";
+
+}
 
 ?>
