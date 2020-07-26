a18c542d3f5a058424eb35e22f0be3e4db244614
Jonathan Tan <jmt26@njit.edu>
2020-07-26 18:20:11 -0400

    Still working out bugs

--- public_html/OrionBank/checking.php
+++ public_html/OrionBank/checking.php
@@ -16,11 +16,14 @@ if (isset($_GET['account'])){
         $stmt = $db->prepare($query);
         $stmt->execute([":account_number" => $account_number]);
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        echo $result;
     } catch (Exception $e) {
         echo $e->getMessage();
     }
 
-    if(isset($result) && count($result) == 1){
+    if(isset($result)){
+        $this_account = $result[0];
         echo "<h5>Account Number: </h5>";
         echo "<h5>" . $result["account_number"] . "</h5></br>";
         echo "<h5>Account Type: </h5>";
@@ -47,8 +50,13 @@ if (isset($_GET['account'])){
     <input type=\"date\" id=\"startdate\" name=\"startdate\">
     <label for=\"enddate\">End Date:</label>
     <input type=\"date\" id=\"enddate\" name=\"enddate\">
-    <label for=\"lname\">How many results?</label><br>
-    <input type=\"int\" id=\"result_count\" name=\"result_count\">
+    <label for=\"result_num\">Results</label>
+    <select name=\"result_num\" id=\"result_num\">
+        <option value=\"10\">10</option>
+        <option value=\"25\">25</option>
+        <option value=\"100\">100</option>
+        <option value=\"All\" selected>All</option>
+    </select>
     <input type=\"submit\" value='Submit'>
     </form>
     ";
