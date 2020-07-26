ad4cf2983335229a99c2362f2c4ddb8bb7dc5742
Jonathan Tan <jmt26@njit.edu>
2020-07-26 18:15:21 -0400

    update scope

--- public_html/OrionBank/checking.php
+++ public_html/OrionBank/checking.php
@@ -9,9 +9,7 @@ echo "<h4>" . $_SESSION["user"]["first_name"] . " " . $_SESSION["user"]["last_na
 if (isset($_GET['account'])){
     $account_number = $_GET['account'];
     echo $account_number;
-
     $query = file_get_contents("queries/SELECT_TABLE_ACCOUNTS_ACCOUNTNUM.sql");
-
     $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
     try {
         $db = new PDO($connection_string, $dbuser, $dbpass);
@@ -21,31 +19,29 @@ if (isset($_GET['account'])){
     } catch (Exception $e) {
         echo $e->getMessage();
     }
-}
 
-if(isset($result) && count($result) == 1){
-    echo "<h5>Account Number: </h5>";
-    echo "<h5>" . $result["account_number"] . "</h5></br>";
+    if(isset($result) && count($result) == 1){
+        echo "<h5>Account Number: </h5>";
+        echo "<h5>" . $result["account_number"] . "</h5></br>";
+        echo "<h5>Account Type: </h5>";
+        echo "<h5>";
+        switch ($result["account_type"]) {
+            case 1:
+                echo "Checking";
+                break;
+            case 2:
+                echo "Savings";
+                break;
+            case 3:
+                echo "Loan";
+                break;
+            default:
+                echo "There is an error";
+                break;
+        };
+        echo "</h5></br>";
 
-    echo "<h5>Account Type: </h5>";
-    echo "<h5>";
-    switch ($result["account_type"]) {
-        case 1:
-            echo "Checking";
-            break;
-        case 2:
-            echo "Savings";
-            break;
-        case 3:
-            echo "Loan";
-            break;
-        default:
-            echo "There is an error";
-            break;
-    };
-    echo "</h5></br>";
-
-    echo "
+        echo "
     <form method='post'>
     <label for=\"startdate\">Start Date:</label>
     <input type=\"date\" id=\"startdate\" name=\"startdate\">
@@ -56,6 +52,6 @@ if(isset($result) && count($result) == 1){
     <input type=\"submit\" value='Submit'>
     </form>
     ";
+    }
 }
-
 ?>
