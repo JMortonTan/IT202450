53247e2db83ef34accc91a1867fc96f3e047d78f
Jonathan Tan <jmt26@njit.edu>
2020-07-27 13:25:26 -0400

    Transactions are now posting. need to update total

--- public_html/OrionBank/queries/DEPOSIT.sql
+++ public_html/OrionBank/queries/DEPOSIT.sql
@@ -1,2 +1,4 @@
-INSERT INTO Transactions (account_src, account_dest, amount) VALUES(:account_src, :account_dest, :amount);
-INSERT INTO Transactions (account_src, account_dest, amount) VALUES(:account_dest, :account_src, :negamount);
+INSERT INTO Transactions (account_src, account_dest, amount, memo) VALUES(:account_src, :account_dest, :amount, "DEPOSIT IN");
+INSERT INTO Transactions (account_src, account_dest, amount, memo) VALUES(:account_dest, :account_src, :negamount, "DEPOSIT OUT");
+UPDATE Accounts SET balance = balance + :amount WHERE :account_src = account_number;
+UPDATE Accounts SET balance = balance - :amount WHERE :account_dest = account_number;
