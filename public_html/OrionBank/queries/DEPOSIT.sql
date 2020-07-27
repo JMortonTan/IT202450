ec8d3934af39c42476adabea134883661de733a0
Jonathan Tan <jmt26@njit.edu>
2020-07-27 12:03:45 -0400

    attempt sql script with only transaction table commands.

--- public_html/OrionBank/queries/DEPOSIT.sql
+++ public_html/OrionBank/queries/DEPOSIT.sql
@@ -1,4 +1,2 @@
 INSERT INTO Transactions (`account_src`,`account_dest`,`amount`,`memo`) VALUES(:account_src, :account_destination, :amount, "Deposit IN");
 INSERT INTO Transactions (`account_src`,`account_dest`,`amount`,`memo`) VALUES(:account_destination, :account_src, :negamount, "Deposit OUT");
-UPDATE Accounts SET balance = balance + :amount WHERE :account_number_src = account_number;
-UPDATE Accounts SET balance = balance - :amount WHERE :account_number_dest = account_number;
