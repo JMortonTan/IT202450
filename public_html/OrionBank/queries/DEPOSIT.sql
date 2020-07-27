995625ca1b92336eed6cb0f8e4553c351c262792
Jonathan Tan <jmt26@njit.edu>
2020-07-27 12:11:34 -0400

    one sql statement at a time

--- public_html/OrionBank/queries/DEPOSIT.sql
+++ public_html/OrionBank/queries/DEPOSIT.sql
@@ -1,4 +1,1 @@
-INSERT INTO Transactions (`account_src`,`account_dest`,`amount`,`memo`) VALUES(:account_src, :account_dest, :amount, "Deposit IN");
-INSERT INTO Transactions (`account_src`,`account_dest`,`amount`,`memo`) VALUES(:account_dest, :account_src, :negamount, "Deposit OUT");
-UPDATE Accounts SET balance = balance + :amount WHERE :account_number_src = account_number;
-UPDATE Accounts SET balance = balance - :amount WHERE :account_number_dest = account_number;
+INSERT INTO Transactions (account_src,account_dest,amount,memo) VALUES(:account_src, :account_dest, :amount, "Deposit IN");
