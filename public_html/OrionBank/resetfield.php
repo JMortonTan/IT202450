2d9a1c415e463359d506506a0e3f8c2466e8b93a
JMortonTan <jtan@MacBook-Pro.local>
2020-08-03 18:34:42 -0400

    fixed ?> typo

--- public_html/OrionBank/resetfield.php
+++ public_html/OrionBank/resetfield.php
@@ -83,10 +83,8 @@ if($logged_in){
                 //negative (in the past)
                 setcookie(session_name(), '', time() - 42000,
                     $params["path"], $params["domain"],
-                    $params["secure"], $params["httponly"]
-                );
-}
-?>
+                    $params["secure"], $params["httponly"]);
+            }
         }
 
     }
